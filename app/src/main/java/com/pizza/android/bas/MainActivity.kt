b0d811a267b13137de8f9826ca87c88f88aa69cd
Austin Scott <12504656+Austin-Scott@users.noreply.github.com>
2020-02-10 16:54:59 -0600

    First commit

---
+++ app/src/main/java/com/pizza/android/bas/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pizza.android.bas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
