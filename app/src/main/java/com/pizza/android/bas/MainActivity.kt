d630a7ffc57345212b51302555fb7b0c2c924a43
Austin Scott <12504656+Austin-Scott@users.noreply.github.com>
2020-02-24 16:47:29 -0600

    Start adding fragments and navigation (WIP)

--- app/src/main/java/com/pizza/android/bas/MainActivity.kt
+++ app/src/main/java/com/pizza/android/bas/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.pizza.android.bas
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
+import androidx.navigation.get
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LoginFragment.OnFragmentInteractionListener{
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawerLayout = findViewById(R.id.drawerLayout)
+        navController = this.findNavController(R.id.nav_host_fragment)
+
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, _ ->
+            if (nd.id != nc.graph.startDestination) {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
     }
 }
